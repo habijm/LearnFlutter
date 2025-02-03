5b77632f1726edfd7fa5d4ad27761f92ab20d4de
habijm <habijiyan111@gmail.com>
2025-02-03 13:10:49 +0700

    Belajar TextFormField

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,27 @@ class AplikasiSaya extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Aplikasi Flutter Saya'),
-          
+          title: Text('Aplikasi Flutter Saya',
+              style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blue, // Ganti dengan warna yang Anda inginkan
         ),
-        body: Center(
-          child: Text('Hello World!'),
+        body: TextFormField(
+          //obscureText: true,
+          //initialValue: 'andy',
+          maxLength: 12,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25),
+              borderSide: BorderSide(color: Colors.blue),
+            ),
+            filled: true,
+            fillColor: const Color.fromARGB(255, 217, 238, 255),
+            hintText: 'Masukkan nama Anda',
+            labelText: 'Nama',
+            helperText: 'Masukkan hanya 10 karakter',
+            prefixIcon: Icon(Icons.supervisor_account), // Icon di sebelah kiri
+            //suffixIcon: Icon(Icons.close), // Icon di sebelah kanan
+          ),
         ),
       ),
     );
