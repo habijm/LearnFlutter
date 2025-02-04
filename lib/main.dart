cc598cb9970d82b56bac8323660102cf0b0c7280
habijm <habijiyan111@gmail.com>
2025-02-04 12:29:10 +0700

    belajar flutter: container

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,31 @@ class AplikasiSaya  extends StatelessWidget {
               style: TextStyle(color: Colors.white)),
           backgroundColor: Colors.blue, // Ganti dengan warna yang Anda inginkan
         ),
-        body: Center(
-          child: new Text('Hello World!'),
-         
+        body: Container(
+          child: Center(
+            child: new Text('Hello World!'),
+
+          ),
+          decoration: BoxDecoration(
+            color: Colors.blue[300],
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.red,
+                width: 2.0,
+              ),
+              top: BorderSide(
+                color: Colors.red,
+                width: 2.0,
+              ),
+              
+            )
+          ),
+          // height: double.infinity,
+          // width: double.infinity, ukuran menyesuaikan layar
+          height: 150,
+          width: 350,
+          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+         margin: EdgeInsets.all(20.0),
         ),
       ),
     );
